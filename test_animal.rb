97ca9ab97c63d3e6eef4080757f452fed03b91aa
Rodney Gelera <rodney.gelera@shopify.com>
2021-06-11 10:13:30 -0700

    forgot test file

---
+++ test_animal.rb
@@ -0,0 +1,15 @@
+require './dog'
+require './fish'
+require './bird'
+
+lucky = Dog.new("Lucky", "brown", ["beef", "chicken", "carrot"], "orange")
+birb = Bird.new("Birb", "green", ["nuts"], "small")
+nemo = Fish.new("Nemo", "orange", ["algae"], "fresh")
+
+puts lucky.print
+puts birb.print { " #{birb.name} is a #{birb.size} bird." }
+puts nemo.print
+
+puts lucky.eat("broccoli")
+lucky.add_to_diet("broccoli")
+puts lucky.eat("broccoli")
